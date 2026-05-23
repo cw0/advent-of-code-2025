d638df0542d8777c2bf124f343fcd88f423b2bb0
cw0 <cwilson8706@gmail.com>
2026-05-23 20:38:01 +0000

    feat: something about python is stopping this from working

--- day-1/main.py
+++ day-1/main.py
@@ -2,23 +2,20 @@
 # this is a circularly linked list problem first we need to define how to move in our circularly linked list
 
 # just to save time later the run command is uv run main.py from the day-1 directory
-    
-def left(current_value, distance):
+   
+def left(current_value, zero_count, distance):
     temp_value = current_value - distance
     while (temp_value < 0):
-        temp_value = temp_value + 100
-    # if (temp_value < 0):
-    #     return 100 + temp_value
-    # else:
+        zero_count = zero_count + 1
+        temp_value += 100
     return temp_value
 
-def right(current_value, distance):
+def right(current_value, zero_count, distance):
     temp_value = current_value + distance
     while (temp_value > 99):
-        temp_value = temp_value - 100
-    # if (temp_value > 99):
-    #     return temp_value - 100
-    # else:
+        zero_count = zero_count + 1
+        temp_value -= 100
+        print("times looped:", zero_count)
     return temp_value
 
 def main():
@@ -35,12 +32,12 @@ def main():
         distance = "".join(instruction_list)
         
         if direction == 'R':
-            current_value = right(current_value, int(distance))
+            current_value = right(current_value, zero_count, int(distance))
         else:
-            current_value = left(current_value, int(distance))
+            current_value = left(current_value, zero_count, int(distance))
 
         if current_value == 0:
-            zero_count += 1
+            zero_count = zero_count + 1
 
     print('password:', zero_count)
 
